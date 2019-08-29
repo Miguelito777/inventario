673237678201e5fdd6242ddab4b5177961fc2a0d
Initwebapps <initwebapps@gmail.com>
2019-08-28 23:42:18 -0600

    template en blanco

--- Fronent/Inventario/src/app/layouts/categoria/categoria.component.ts
+++ Fronent/Inventario/src/app/layouts/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from './categoria.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -8,18 +9,67 @@ import { CategoriaService } from './categoria.service';
 })
 export class CategoriaComponent implements OnInit {
   Categorias:any=[];
+  categoria: FormGroup;
   constructor(
-    private api:CategoriaService
+    private api:CategoriaService,
+    private fb: FormBuilder
   ) { 
 
   }
 
   ngOnInit() {
-    this.api.getCategorias().subscribe(
+    this.api.getAll().subscribe(
       data=>{
         this.Categorias = data;
       }
-    )
-  }
+    );
 
+    this.categoria = this.fb.group({
+      categoria: ['', [Validators.required, Validators.minLength(2)]],
+      id:[null]
+    });
+  }
+  onSubmit(dataForm){
+    if(dataForm.id == null){
+      this.api.postOne(dataForm).subscribe(
+        data=>{
+          this.Categorias.push(data);
+          this.categoria.controls.categoria.setValue('');
+        }
+      );
+    }else{
+      this.api.putOne(dataForm).subscribe(
+        data=>{
+          //this.Categorias.push(data);
+          for(var i in this.Categorias){
+            if(this.Categorias[i].id == data.id){
+              this.Categorias[i] = data;
+            }
+          }
+          this.categoria.controls.categoria.setValue('');
+          this.categoria.controls.id.setValue(null);
+        }
+      );
+    }
+  }
+  edit(item){
+    this.categoria.controls.categoria.setValue(item.categoria);
+    this.categoria.controls.id.setValue(item.id);
+  }
+  delete(id){
+    let deleteItem = false;
+    for(var i in this.Categorias){
+      if(this.Categorias[i].id == id){
+        this.Categorias.splice(i,1);
+        deleteItem = true;
+      }
+    }
+    if(deleteItem){
+      this.api.deleteOne(id).subscribe(
+        data=>{
+          console.log(data);
+        }
+      );
+    }
+  } 
 }
--- Fronent/Inventario/src/app/layouts/categoria/categoria.service.ts
+++ Fronent/Inventario/src/app/layouts/categoria/categoria.service.ts
@@ -16,10 +16,28 @@ export class CategoriaService {
   private getUrl(base, service) {   
     this.finallyUrl=base+service;
   }
-  getCategorias(): Observable<any> {    
+  getAll(): Observable<any> {    
     return this.http.get<any>(this.url+'mostrarcat')
     .pipe(
       catchError(this.handleError('mostrarcat', undefined))
+    );
+  }
+  postOne(item): Observable<any> {    
+    return this.http.post<any>(this.url+'cat',item)
+    .pipe(
+      catchError(this.handleError('cat', undefined))
+    );
+  }
+  putOne(item): Observable<any> {    
+    return this.http.put<any>(this.url+'actualizarcat/'+item.id,item)
+    .pipe(
+      catchError(this.handleError('actualizarcat', undefined))
+    );
+  }
+  deleteOne(id): Observable<any> {    
+    return this.http.delete<any>(this.url+'eliminarcat/'+id)
+    .pipe(
+      catchError(this.handleError('eliminarcat', undefined))
     );
   }
   private handleError<T>(operation = 'operation', result?: T) {
