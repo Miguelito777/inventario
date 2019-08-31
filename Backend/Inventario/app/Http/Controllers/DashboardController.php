d2c7ddbd8379f1a997beff469d78622c13da100a
Initwebapps <initwebapps@gmail.com>
2019-08-31 15:51:05 -0600

    Motivo

--- Backend/Inventario/app/Http/Controllers/DashboardController.php
+++ Backend/Inventario/app/Http/Controllers/DashboardController.php
@@ -36,16 +36,17 @@ class DashboardController extends Controller
     return response()->json(TT_MOVIMIENTO::all()->where('idtipomovimiento','=',2)->count());
    }
 
-   public function getIngresos($idTipoIngreso){
+   public function getIngresos($idMotivo){
         $ingresos = DB::table('tt_movimiento as mov')
             ->select('mov.created_at as fecha_ingreso', 'mov.cantidad', 'mov.precio', 'prod.nombre', 'est.codigo as estanteria', 'pas.codigo as pasillo', 'bod.bodega')
             ->join('tt_producto as prod','mov.id_producto', '=', 'prod.id')
             ->join('tt_estanterias as est','mov.id_estanteria', '=', 'est.id')
             ->join('tt_pasillos as pas','est.idpasillo', '=', 'pas.id')
             ->join('tc_bodegas as bod','pas.idbodega', '=', 'bod.id')
-            ->join('tt_tipomov as tm', function ($join)use($idTipoIngreso) {
-                $join->on('mov.idtipomovimiento', '=', 'tm.id')
-                     ->where('mov.idtipomovimiento', '=', $idTipoIngreso);
+            ->join('tt_tipomov as tm', 'mov.idtipomovimiento', '=', 'tm.id')
+            ->join('tc_motivo as mt', function ($join)use($idMotivo) {
+                $join->on('tm.id_motivo', '=', 'mt.id')
+                     ->where('mt.id', '=', $idMotivo);
             })->get();
         return response()->json($ingresos);
     }
